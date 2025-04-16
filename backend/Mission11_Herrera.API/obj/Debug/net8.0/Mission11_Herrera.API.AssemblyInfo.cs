48f096202d4bd73d08d303467fb857d65b5da70b
davisherrera <daviseherrera05@gmail.com>
2025-04-16 17:27:51 -0600

    Commit made from VS Code Azure Static Web Apps

--- backend/Mission11_Herrera.API/obj/Debug/net8.0/Mission11_Herrera.API.AssemblyInfo.cs
+++ backend/Mission11_Herrera.API/obj/Debug/net8.0/Mission11_Herrera.API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Mission11_Herrera.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7e7bd654352193fec8047fcd5fcc57d66891e5dd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+31857d5136ac44df4bb5c3476aa0783addfe008d")]
 [assembly: System.Reflection.AssemblyProductAttribute("Mission11_Herrera.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Mission11_Herrera.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
